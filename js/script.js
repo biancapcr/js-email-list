692592a4817b9b124f5a95b68c1918f1520aa98d
biancapcr <biancacristina.pocora@gmail.com>
2025-07-20 15:24:50 +0200

    [add] ciclo for

--- js/script.js
+++ js/script.js
@@ -2,3 +2,22 @@
 const emailList = document.getElementById("emailList");
 // Seleziono il bottone che rigenera le email
 const refreshBtn = document.getElementById("refreshBtn");
+// Funzione che carica 10 email casuali dall'API
+function loadEmails() {
+  // Svuoto la lista prima di inserire nuove voci
+  emailList.innerHTML = "";
+
+  // Eseguo 10 chiamate axios in un ciclo
+  for (let i = 0; i < 10; i++) {
+    axios
+      .get("https://flynn.boolean.careers/exercises/api/random/mail")
+      .then((resp) => {
+        // Estraggo l'email dalla risposta
+        const email = resp.data.response;
+        // Creo un nuovo <li> con la classe di Bootstrap e l'email
+        const listItem = `<li class="list-group-item">${email}</li>`;
+        // Lo appendo alla lista
+        emailList.innerHTML += listItem;
+      });
+  }
+}

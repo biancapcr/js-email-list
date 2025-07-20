aa1575f6a5e6010d92e769491e34c92cad8f6323
biancapcr <biancacristina.pocora@gmail.com>
2025-07-20 15:17:50 +0200

    [add] const

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// Seleziono l'elemento <ul> dove inserire le email
+const emailList = document.getElementById("emailList");
+// Seleziono il bottone che rigenera le email
+const refreshBtn = document.getElementById("refreshBtn");
